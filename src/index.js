8e7128d779d66f5c3860b351409c4cb534ed9e09
John <johnfarris217@gmail.com>
2022-12-08 14:26:47 -0600

    challenge basics

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    newTask(e.target.new_todo.value);
+    form.reset();
+  })
 });
+
+function newTask(task) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteTask);
+  btn.textContent = 'x';
+  li.textContent = task + '  ';
+  li.appendChild(btn);
+  document.querySelector('ol').appendChild(li);
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove();
+}
